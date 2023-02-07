11a89376f897fdeff4fba23935935f4ced128c42
Bijo Varghese <bijo.varghese@ymail.com>
2023-02-07 00:57:05 -0500

    az

--- terraform-aws-sslo/vpc.tf
+++ terraform-aws-sslo/vpc.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "security_vpc" {
   cidr_block           = var.vpc_cidrs["vpc1"]
   enable_dns_hostnames = "true"
   tags = {
-    Name = "${var.prefix}-sslo-securitystack"
+    Name = "${var.prefix}_security_vpc"
   }
 }
 
@@ -12,6 +12,6 @@ resource "aws_vpc" "app_vpc" {
   cidr_block           = var.vpc_cidrs["vpc2"]
   enable_dns_hostnames = "true"
   tags = {
-    Name = "${var.prefix}-sslo-appstack"
+    Name = "${var.prefix}_app_vpc"
   }
 }
