8a0e3db2d2d09ffdb48599bc3492b8c6783db88e
Bijo Varghese <bijo.varghese@ymail.com>
2023-02-09 10:38:42 -0500

    gwlb

---
+++ terraform-aws-sslo/gwlb.tf
@@ -0,0 +1,71 @@
+locals {
+  bigip = {
+    "bigip_sslo_az1" = "${aws_instance.sslo_az1.id}",
+    "bigip_sslo_az2" = "${aws_instance.sslo_az2.id}"
+  }
+}
+
+##################GWLB#################
+
+resource "aws_lb" "sslo_gwlb" {
+  internal           = false
+  load_balancer_type = "gateway"
+  subnets            = [aws_subnet.external_az1.id, aws_subnet.external_az2.id]
+
+  tags = {
+    Name  = "${var.prefix}-gwlb"
+    #Owner = var.resourceOwner
+  }
+}
+
+resource "aws_lb_target_group" "sslo_tg" {
+  name        = "${var.prefix}-tg"
+  port        = 6081
+  protocol    = "GENEVE"
+  target_type = "instance"
+  vpc_id      = aws_vpc.security_vpc.id
+
+  health_check {
+    protocol = "TCP"
+    port     = 80
+    #    matcher  = "200-399"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "sslo_tg_attachment" {
+  #count            = 2
+  for_each         = local.bigip
+  target_group_arn = aws_lb_target_group.sslo_tg.arn
+  target_id        = each.value
+}
+
+
+resource "aws_vpc_endpoint_service" "sslo_gwlb_eps" {
+  acceptance_required        = false
+  gateway_load_balancer_arns = [aws_lb.sslo_gwlb.arn]
+}
+
+resource "aws_lb_listener" "sslo_gwlb_listener" {
+  load_balancer_arn = aws_lb.sslo_gwlb.id
+
+  default_action {
+    target_group_arn = aws_lb_target_group.sslo_tg.id
+    type             = "forward"
+  }
+}
+
+resource "aws_vpc_endpoint" "sslo_gwlb_ep_az1" {
+  #count             = var.createGwlbEndpoint ? 1 : 0
+  service_name      = aws_vpc_endpoint_service.sslo_gwlb_eps.service_name
+  subnet_ids        = aws_subnet.gwlbe_az1.id
+  vpc_endpoint_type = "GatewayLoadBalancer"
+  vpc_id            = aws_vpc.security_vpc.id
+}
+
+resource "aws_vpc_endpoint" "sslo_gwlb_ep_az2" {
+  #count             = var.createGwlbEndpoint ? 1 : 0
+  service_name      = aws_vpc_endpoint_service.sslo_gwlb_eps.service_name
+  subnet_ids        = aws_subnet.gwlbe_az2.id
+  vpc_endpoint_type = "GatewayLoadBalancer"
+  vpc_id            = aws_vpc.security_vpc.id
+}
